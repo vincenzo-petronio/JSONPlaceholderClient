d70240ef3118e7d7e1acb407c55d195b9f172fb0
dev PA <vincenzo.petronio@gmail.com>
2016-07-21 15:01:27 +0200

    FIX

--- app/src/main/java/it/localhost/app/mobile/jsonplaceholderclient/ui/activity/MainActivity.java
+++ app/src/main/java/it/localhost/app/mobile/jsonplaceholderclient/ui/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package it.localhost.app.mobile.jsonplaceholderclient.ui.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
